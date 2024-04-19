6cfec9ced6f1392a97048479ec72be63a8134550
구인모 <ttrr7eek@naver.com>
2024-04-19 15:41:30 +0900

    완성

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ import pandas as pd
 data = pd.read_csv('ThoraricSurgery3.csv')
 data = data.dropna()
 
-x데이터 = data.iloc[:, 0:15].value.tolist()
-y데이터 = data.iloc[:, 15].value.tolist()
+x데이터 = data.iloc[:, 0:15].values.tolist()
+y데이터 = data.iloc[:, 15].values.tolist()
 
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(64, activation='tanh'),
+    tf.keras.layers.Dense(128, activation='tanh'),
+    tf.keras.layers.Dense(1, activation='sigmoid'),
+])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'] )
+
+model.fit( np.array(x데이터), np.array(y데이터), epochs=10 )
